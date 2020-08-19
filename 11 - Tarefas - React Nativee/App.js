29123d4bc3ed4dc5cb867927fefed21e20012f04
André Aragon <andregaragon@gmail.com>
2020-08-19 17:01:28 -0300

    React Native

---
+++ 11 - Tarefas - React Nativee/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from "./Styles";
+
+export default function App() {
+  return (
+    <View style={styles.body}>
+      <View style={styles.container}>
+        <Text style={[styles.h1, styles.smallCaps]}>Noticias</Text>
+ asdasdasd
+      </View>
+    </View>
+  );
+}
---
+++ 11 - Tarefas - React Nativee/Styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    body: {
+        margin: 0,
+        padding: 0,
+        backgroundColor: "lightgray",
+    },
+
+    h1: {
+        textAlign: "center",
+        fontSize: "1.8em",
+    },
+
+    container: {
+        paddingTop: "72px",
+    },
+
+    smallCaps: {
+        fontVariant: ["small-caps"],
+    }
+})
